ebf649de38fa289f7a72d32065f5a7e5426b0a19
Yokeshwar <yokeshwar20@gmail.com>
2026-01-21 20:11:26 +0530

    Delete src/src directory

--- src/src/main/java/com/letschat/mvp_1/Repositories/AssignmentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.letschat.mvp_1.Repositories;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import com.letschat.mvp_1.Models.AssignmentInfo;
-
-public interface AssignmentRepo extends  ReactiveCrudRepository<AssignmentInfo, Long>{
-    
-}
